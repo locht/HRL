dadd424e39e1757548b90af3d5caa1dcc86c61ad
chiennv <chiennv@tinhvan.com>
2019-04-11 08:34:19 +0700

    AttachFilesDTO

---
+++ ProfileDAL/BusinessObjects/Setting/AttachFilesDTO.vb
@@ -0,0 +1,8 @@
+﻿Public Class AttachFilesDTO
+    Public Property ID As Decimal
+    Public Property FK_ID As Decimal
+    Public Property ATTACHFILE_NAME As String
+    Public Property FILE_PATH As String
+    Public Property CONTROL_NAME As String
+    Public Property FILE_TYPE As String
+End Class
