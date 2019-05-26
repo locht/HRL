204f1dd738a36c4c75c22d39964951149459507e
chiennv <chiennv@tinhvan.com>
2019-05-26 21:58:04 +0700

    FIX IMPORT DATA

--- Attendance/Modules/Attendance/Business/ctrlSwipeDataDownload.ascx.vb
+++ Attendance/Modules/Attendance/Business/ctrlSwipeDataDownload.ascx.vb
@@ -283,14 +283,18 @@ Public Class ctrlSwipeDataDownload
                 obj.MACHINE_TYPE = cbMachine_Type.SelectedValue
             End If
             Dim Sorts As String = rglSwipeDataDownload.MasterTableView.SortExpressions.GetSortString()
-            If Not isFull Then
-                If Sorts IsNot Nothing Then
-                    Me.SWIPE_DATA = rep.GetSwipeData(obj, rglSwipeDataDownload.CurrentPageIndex, rglSwipeDataDownload.PageSize, MaximumRows, Sorts)
-                Else
-                    Me.SWIPE_DATA = rep.GetSwipeData(obj, rglSwipeDataDownload.CurrentPageIndex, rglSwipeDataDownload.PageSize, MaximumRows)
-                End If
+            If Not IsDate(rdStartDate.SelectedDate) And Not IsDate(rdEndDate.SelectedDate) Then
+                Me.SWIPE_DATA = New List(Of AT_SWIPE_DATADTO)
             Else
-                Return rep.GetSwipeData(obj).ToTable()
+                If Not isFull Then
+                    If Sorts IsNot Nothing Then
+                        Me.SWIPE_DATA = rep.GetSwipeData(obj, rglSwipeDataDownload.CurrentPageIndex, rglSwipeDataDownload.PageSize, MaximumRows, Sorts)
+                    Else
+                        Me.SWIPE_DATA = rep.GetSwipeData(obj, rglSwipeDataDownload.CurrentPageIndex, rglSwipeDataDownload.PageSize, MaximumRows)
+                    End If
+                Else
+                    Return rep.GetSwipeData(obj).ToTable()
+                End If
             End If
             rglSwipeDataDownload.VirtualItemCount = MaximumRows
             rglSwipeDataDownload.DataSource = Me.SWIPE_DATA
@@ -353,8 +357,12 @@ Public Class ctrlSwipeDataDownload
                 Case "IMPORT_TEMP"
                     ctrlUpload.Show()
                 Case "IMPORT"
-                    ctrlUpload1.Show()
-
+                    If IsNumeric(cbMachine_Type.SelectedValue) Then
+                        ctrlUpload1.Show()
+                    Else
+                        ShowMessage(Translate(CommonMessage.MESSAGE_NOT_CHOOSE_MACHINE_TYPE), NotifyType.Warning)
+                        Exit Sub
+                    End If
             End Select
             _myLog.WriteLog(_myLog._info, _classPath, method,
                                                            CLng(DateTime.UtcNow.Subtract(startTime).TotalSeconds).ToString(), Nothing, "")
