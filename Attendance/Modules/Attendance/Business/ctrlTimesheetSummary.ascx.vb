5ca29ee020aa6bfeecf7ffe93b515e685007975c
donghpt <donghpt@tinhvan.com>
2019-05-29 15:19:21 +0700

    Fix tinh gio OT + case config

--- Attendance/Modules/Attendance/Business/ctrlTimesheetSummary.ascx.vb
+++ Attendance/Modules/Attendance/Business/ctrlTimesheetSummary.ascx.vb
@@ -321,12 +321,23 @@ Public Class ctrlTimesheetSummary
                     Dim _param = New ParamDTO With {.PERIOD_ID = Decimal.Parse(cboPeriod.SelectedValue), _
                                                     .ORG_ID = Decimal.Parse(ctrlOrg.CurrentValue),
                                                     .IS_DISSOLVE = ctrlOrg.IsDissolve}
-                    If rep.CAL_TIME_TIMESHEET_MONTHLY(_param, "ctrlTimesheetSummary_case1", lsEmployee) Then
-                        Refresh("UpdateView")
+                    If getSE_CASE_CONFIG("ctrlTimesheetSummary_case1") > 0 Then
+                        If rep.CAL_TIME_TIMESHEET_MONTHLY(_param, "ctrlTimesheetSummary_case1", lsEmployee) Then
+                            Refresh("UpdateView")
+                        Else
+                            ShowMessage(Translate(CommonMessage.MESSAGE_TRANSACTION_FAIL), NotifyType.Success)
+                            Exit Sub
+                        End If
                     Else
-                        ShowMessage(Translate(CommonMessage.MESSAGE_TRANSACTION_FAIL), NotifyType.Success)
-                        Exit Sub
+                        If rep.CAL_TIME_TIMESHEET_MONTHLY(_param, "", lsEmployee) Then
+                            Refresh("UpdateView")
+                        Else
+                            ShowMessage(Translate(CommonMessage.MESSAGE_TRANSACTION_FAIL), NotifyType.Success)
+                            Exit Sub
+                        End If
                     End If
+                    
+                    
                 Case TOOLBARITEM_EXPORT
                     Using xls As New ExcelCommon
                         Dim dtDatas As DataTable
