e23b9cfc2c4948ec7ad64497acbfdd9da8ac2ff6
TungLD <tungld@tinhvan.com>
2019-06-13 09:00:05 +0700

    Modify Print_CV

--- Profile/Service References/ProfileBusiness/Reference.vb
+++ Profile/Service References/ProfileBusiness/Reference.vb
@@ -10966,6 +10966,9 @@ Namespace ProfileBusiness
         Private IMAGE_BINARYField() As Byte
         
         <System.Runtime.Serialization.OptionalFieldAttribute()>  _
+        Private IMAGE_URLField As String
+        
+        <System.Runtime.Serialization.OptionalFieldAttribute()>  _
         Private IS_HISTORYField As Boolean
         
         <System.Runtime.Serialization.OptionalFieldAttribute()>  _
@@ -11520,6 +11523,19 @@ Namespace ProfileBusiness
                 If (Object.ReferenceEquals(Me.IMAGE_BINARYField, value) <> true) Then
                     Me.IMAGE_BINARYField = value
                     Me.RaisePropertyChanged("IMAGE_BINARY")
+                End If
+            End Set
+        End Property
+        
+        <System.Runtime.Serialization.DataMemberAttribute()>  _
+        Public Property IMAGE_URL() As String
+            Get
+                Return Me.IMAGE_URLField
+            End Get
+            Set
+                If (Object.ReferenceEquals(Me.IMAGE_URLField, value) <> true) Then
+                    Me.IMAGE_URLField = value
+                    Me.RaisePropertyChanged("IMAGE_URL")
                 End If
             End Set
         End Property
@@ -33856,6 +33872,9 @@ Namespace ProfileBusiness
         Private ID_PLACEField As String
         
         <System.Runtime.Serialization.OptionalFieldAttribute()>  _
+        Private ID_PLACE_NAMEField As String
+        
+        <System.Runtime.Serialization.OptionalFieldAttribute()>  _
         Private MARITAL_STATUSField As System.Nullable(Of Decimal)
         
         <System.Runtime.Serialization.OptionalFieldAttribute()>  _
@@ -34057,6 +34076,19 @@ Namespace ProfileBusiness
                 If (Object.ReferenceEquals(Me.ID_PLACEField, value) <> true) Then
                     Me.ID_PLACEField = value
                     Me.RaisePropertyChanged("ID_PLACE")
+                End If
+            End Set
+        End Property
+        
+        <System.Runtime.Serialization.DataMemberAttribute()>  _
+        Public Property ID_PLACE_NAME() As String
+            Get
+                Return Me.ID_PLACE_NAMEField
+            End Get
+            Set
+                If (Object.ReferenceEquals(Me.ID_PLACE_NAMEField, value) <> true) Then
+                    Me.ID_PLACE_NAMEField = value
+                    Me.RaisePropertyChanged("ID_PLACE_NAME")
                 End If
             End Set
         End Property
