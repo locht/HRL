474861312c28ffd9b362d572b622863b14451515
quanhsh <quanhsh@tinhvan.com>
2019-07-02 14:01:37 +0700

    config trong màn hình quản lý phép năm với lại máy châm công

--- Attendance/Modules/Attendance/Business/ctrlSwipeDataDownload.ascx.vb
+++ Attendance/Modules/Attendance/Business/ctrlSwipeDataDownload.ascx.vb
@@ -137,6 +137,10 @@ Public Class ctrlSwipeDataDownload
             Dim startTime As DateTime = DateTime.UtcNow
             log = LogHelper.GetUserLog
             InitControl()
+            If Not IsPostBack Then
+                ViewConfig(RadPane1)
+                GirdConfig(rglSwipeDataDownload)
+            End If
             _myLog.WriteLog(_myLog._info, _classPath, method,
                                                            CLng(DateTime.UtcNow.Subtract(startTime).TotalSeconds).ToString(), Nothing, "")
         Catch ex As Exception
