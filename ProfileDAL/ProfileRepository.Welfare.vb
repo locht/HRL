7911ee038e6f586fd3f4966864f2efe264e78f9e
quanhsh <quanhsh@tinhvan.com>
2019-08-22 09:23:06 +0700

    TNG-159 [iProfile > Nghiệp vụ] Quản lý phúc lợi: nghiệp vụ xử lý danh sách nhận phúc lợi

--- ProfileDAL/ProfileRepository.Welfare.vb
+++ ProfileDAL/ProfileRepository.Welfare.vb
@@ -406,13 +406,14 @@ Partial Class ProfileRepository
         Return True
     End Function
 
-    Public Function GET_DETAILS_EMP(ByVal P_ID As Decimal, ByVal P_WELFARE_ID As Decimal, ByVal P_DATE As Date) As DataTable
+    Public Function GET_DETAILS_EMP(ByVal P_ID As Decimal, ByVal P_WELFARE_ID As Decimal, ByVal P_DATE As Date, ByVal log As UserLog) As DataTable
         Try
             Using cls As New DataAccess.QueryData
                 Dim dtData As DataTable = cls.ExecuteStore("PKG_HU_IPROFILE_LIST.GET_DETAILS_EMP",
                                            New With {.P_EMP_ID = P_ID,
                                                      .P_WELFARE_ID = P_WELFARE_ID,
                                                      .P_DATE = P_DATE,
+                                                     .P_USER_NAME = log.Username,
                                                      .P_CUR = cls.OUT_CURSOR})
 
                 Return dtData
