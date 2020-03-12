e4da92bf2b7995da36d4c2daf9443d9bc1f1ff0b
nganle <nganlpt@tinhvan.com>
2020-03-12 09:10:47 +0700

    ACVUSA-58

---
+++ PayrollDAL/BusinessObjects/SalaryGroup/SalaryExRateDTO.vb
@@ -0,0 +1,15 @@
+﻿Public Class SalaryExRateDTO
+    Public Property ID As Decimal
+    Public Property CODE As String
+    Public Property NAME As String
+    Public Property FOREIGN_CURRENCY_TYPE As String
+    Public Property EFFECT_DATE As Date?
+    Public Property REMARK As String
+    Public Property CREATED_DATE As Date?
+    Public Property ORDERS As Decimal?
+    Public Property IS_COEFFICIENT As Boolean?
+    Public Property IS_INCENTIVE As Boolean?
+    Public Property ACTFLG As String
+    Public Property IS_DELETED As Decimal?
+
+End Class
