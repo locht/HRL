1a5f3a10902737ce3814a4ac37d4f7e2fedbd7b9
quanhsh <quanhsh@tinhvan.com>
2020-05-06 17:24:10 +0700

    COMMIT SỬA ACV 217

--- HistaffWebApp/Export.aspx.vb
+++ HistaffWebApp/Export.aspx.vb
@@ -1074,7 +1074,7 @@ Public Class Export
         Try
             Dim dtColName = Session("IMPORTSALARY_COLNAME")
             Dim dtData = Session("IMPORTSALARY_DATACOL")
-            ExportTemplateWithDataCol1("Payroll\Business\TEMP_IMPORT_SALARY_BONUS.xlsx", dtData, dtColName, "TEMP_IMPORTBONUS" & Format(Date.Now, "yyyyMMdd"))
+            ExportTemplateWithDataCol("Payroll\Business\TEMP_IMPORT_SALARY_BONUS.xlsx", dtData, dtColName, "TEMP_IMPORTBONUS" & Format(Date.Now, "yyyyMMdd"))
 
         Catch ex As Exception
             Throw ex
