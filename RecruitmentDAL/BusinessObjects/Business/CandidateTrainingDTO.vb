451bf4a6a44b72351645310b34d3e7a8c4074c50
trankimchi <trankimchi4869@gmail.com>
2020-05-14 08:07:44 +0700

    update

---
+++ RecruitmentDAL/BusinessObjects/Business/CandidateTrainingDTO.vb
@@ -0,0 +1,8 @@
+﻿Public Class CandidateTrainingDTO
+    Public Property CANDIDATE_ID As Decimal
+    Public Property SCHOOL_ID As Decimal?
+    Public Property MAJOR_ID As Decimal?
+    Public Property FROM_DATE As Date?
+    Public Property TO_DATE As Date?
+    Public Property MARK_EDU_ID As Decimal?
+End Class
