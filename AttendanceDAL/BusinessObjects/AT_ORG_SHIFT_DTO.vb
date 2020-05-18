bceafee141edea7880ea60f3903f0fb805848c2a
nganle <nganlpt@tinhvan.com>
2020-05-18 11:44:54 +0700

    Fix lỗi

---
+++ AttendanceDAL/BusinessObjects/AT_ORG_SHIFT_DTO.vb
@@ -0,0 +1,7 @@
+﻿Public Class AT_ORG_SHIFT_DTO
+    Public Property ID As Decimal?
+    Public Property ORG_ID As Decimal?
+    Public Property ORG_NAME As String
+    Public Property SHIFT_CODE As String '' tuong ung voi danh sach ca lam viec
+
+End Class
