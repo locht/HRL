8af851117820a2d9df83725a270a7603b8a46e2b
trankimchi <trankimchi4869@gmail.com>
2020-05-22 11:16:53 +0700

    acv345

---
+++ Profile/Modules/Profile/BusinessObjects/ImageInfoDTO.vb
@@ -0,0 +1,8 @@
+﻿Public Class ImageInfoDTO
+    Public Property EMP_ID As Decimal
+    Public Property EMP_CODE As String
+    Public Property EMP_IMAGE_NAME As String
+    Public Property EMP_IMAGE_BYTE As Byte()
+    Public Property EMP_LINK_IMG As String
+    Public Property ORG_NAME As String
+End Class
