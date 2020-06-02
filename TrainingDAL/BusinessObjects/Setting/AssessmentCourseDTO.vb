eef4d26828f36efbc36fee00e8e67445fc507abd
Phan Thanh Giau <giaupt@tinhvan.com>
2020-06-02 13:53:29 +0700

    BS

---
+++ TrainingDAL/BusinessObjects/Setting/AssessmentCourseDTO.vb
@@ -0,0 +1,15 @@
+﻿Public Class AssessmentCourseDTO
+    Public Property ID As Decimal
+    Public Property YEAR As Decimal
+    Public Property COURSE_ID As Decimal
+    Public Property COURSE_NAME As String
+    Public Property ASSESSMENT_FROM_ID As Decimal
+    Public Property ASSESSMENT_NAME As String
+    Public Property REMARK As String
+    Public Property CREATED_DATE As Date?
+    Public Property CREATED_BY As String
+    Public Property CREATED_LOG As String
+    Public Property MODIFIED_DATE As Date?
+    Public Property MODIFIED_BY As String
+    Public Property MODIFIED_LOG As String
+End Class
