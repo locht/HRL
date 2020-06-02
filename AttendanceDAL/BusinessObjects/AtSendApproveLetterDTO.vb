5908c53395b735d904028e2f0f18d9e8e8aee0a8
tambt <tambt@tinhvan.com>
2020-06-02 14:11:07 +0700

    UPDATE OBJ

---
+++ AttendanceDAL/BusinessObjects/AtSendApproveLetterDTO.vb
@@ -0,0 +1,11 @@
+﻿Public Class AtSendApproveLetterDTO
+    Public Property ID As Decimal
+    Public Property LEAVE_ID As Decimal
+    Public Property USERNAME As String
+    Public Property ACTION_DATE As Date?
+    Public Property ACTION_TYPE As Decimal?
+    Public Property ACTION_STATUS As Decimal?
+    Public Property RUN_START As Date?
+    Public Property RUN_END As Date?
+    Public Property RUN_ROW As Decimal?
+End Class
