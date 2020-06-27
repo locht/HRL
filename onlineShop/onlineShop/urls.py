732be033b64244b5f8a86dc0c4422dee28be4291
locht <thanhloclhu@gmail.com>
2020-06-27 20:07:19 +0700

    1

--- onlineShop/onlineShop/urls.py
+++ onlineShop/onlineShop/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+from django.conf.urls import include, url
+from django.contrib import admin 
+
+from django.conf import settings
+from django.conf.urls.static import static
+ 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'^shopping/', include('shopping.urls')),
+    url(r'^admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
