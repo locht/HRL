4f7e236887512a5a8f2593f23277e970b813741d
locht <thanhloclhu@gmail.com>
2020-06-27 20:41:18 +0700

    1

--- onlineShop/onlineShop/urls.py
+++ onlineShop/onlineShop/urls.py
@@ -24,10 +24,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin 
 
-from django.conf import settings
-from django.conf.urls.static import static
  
 urlpatterns = [
     url(r'^shopping/', include('shopping.urls')),
     url(r'^admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
