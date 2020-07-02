9700b3e3122a60d56447f6bed49d95e8ee80ff4b
locht <locht@tinhvan.com>
2020-07-02 16:40:50 +0700

    1

--- tutorial/account/urls.py
+++ tutorial/account/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetDoneView, PasswordResetView
 
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', LoginView.as_view(template_name='account/login.html'), name='login'),
     url(r'^logout/$', LogoutView.as_view(template_name='account/logout.html'), name='logout'),
-    url(r'^register/$', views.register, name='register')
+    url(r'^register/$', views.register, name='register'),
+    url(r'^profile/$', views.view_profile, name='profile'),
+    url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
+    url(r'^change-password/$', views.change_password, name='change_password'),
+    url(r'^reset-password/$', PasswordResetView.as_view(), name='reset_password'),
+    url(r'^reset-password/done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
 ]
--- tutorial/account/views.py
+++ tutorial/account/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from account.forms import RegistrationForm
+from account.forms import RegistrationForm, EditProfileForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def home(request):
     numbers = [1,2,3,4,5]
     name = 'DevOps'
@@ -22,3 +26,36 @@ def register(request):
 
         args = {'form':form}
         return render(request, 'account/reg_form.html', args)
+@login_required
+def view_profile(request):
+    args = {'user':request.user}
+    return render(request, 'account/profile.html', args)
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'account/edit_profile.html', args)   
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/account/profile')
+        else:
+            return redirect('/account/change-password')
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form': form}
+        return render(request, 'account/change_password.html', args)    
