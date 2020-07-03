1766611629ea09c68e635d5524f0c0c445722afd
locht <locht@tinhvan.com>
2020-07-03 17:10:01 +0700

    1

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -24,5 +24,5 @@ from tutorial import views
 urlpatterns = [
     url(r'^$', views.login_redirect, name='login_redirect'),
     url('admin/', admin.site.urls),
-    url(r'^account/', include ('account.urls')),
+    url(r'^account/', include ('account.urls', namespace='account')),
 ]
