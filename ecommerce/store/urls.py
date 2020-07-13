6b59cee37df4ca102849e5e5327d0b38e0d5a5f5
locht <thanhloclhu@gmail.com>
2020-07-13 23:10:59 +0700

    1

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.store, name='store'),
     url(r'^cart/$', views.cart, name='cart'),
     url(r'^checkout/$', views.checkout, name='checkout'),
-     
+    
+    url(r'^update_item/', views.updateItem, name='update_item'),
 ]
