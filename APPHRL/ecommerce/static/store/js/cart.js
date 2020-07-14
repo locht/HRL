8dbdb37f0ec81225f0851dfba6346a034d1e90fd
locht <locht@tinhvan.com>
2020-07-14 16:57:00 +0700

    chinh sua add to cart done

--- APPHRL/ecommerce/static/store/js/cart.js
+++ APPHRL/ecommerce/static/store/js/cart.js
@@ -17,4 +17,26 @@ for (var i = 0; i < updateBtns.length; i++){
 
 function updateUserOder(productId, action) {
     console.log('User is authenticated, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({
+            'productId':productId,
+            'action':action,
+        })
+    })
+
+    .then((response) =>{
+        return response.json()
+    })
+    .then((data) =>{
+        location.reload()
+        console.log('data:', data)
+    })
 }
